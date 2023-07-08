311c81f5187643fa378517aa89a87ee844c8497e
Bassey <inameti@systemspecs.com.ng>
2023-07-08 18:44:43 +0100

    angela yu day 1 band name generator

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Hello mortal, this is a band name generator")
+city = input("So what city do you hail from ? \n")
+pet=input("what's the name of a pet you've had or wanted to have ?\n")
+
+bandName= city+" "+pet
+print(bandName)
